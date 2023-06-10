50589ffd25785c51f1f3bec67a7b599f830d7474
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2023-06-10 02:45:06 +0100

    update flake.nix
    - no need to be on the unstable channel
    - no need to set default options

--- flake.nix
+++ flake.nix
@@ -1,24 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
+      imports = [haskell-flake.flakeModule];
       perSystem = { self', pkgs, ... }: {
         packages.default = self'.packages.jotform-api-haskell;
         haskellProjects.default = {
-          basePackages = pkgs.haskellPackages;
           devShell = {
-            enable = true;
             tools = haskell: {
               fourmolu = haskell.fourmolu;
             };
-            hlsCheck.enable = true;
           };
         };
       };
